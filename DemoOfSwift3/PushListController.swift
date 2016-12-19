bc10acee928998076fd2ba0abf669963c0e1d6bb
xuzhiwu <anubis_873375@hotmail.com>
2016-12-19 17:31:18 +0800

    add files

---
+++ DemoOfSwift3/PushListController.swift
@@ -0,0 +1,105 @@
+//
+//  PushListController.swift
+//  DemoOfSwift3
+//
+//  Created by 许之午 on 2016/12/19.
+//  Copyright © 2016年 许之午. All rights reserved.
+//
+
+import UIKit
+
+class PushListController: UITableViewController {
+    
+    let cellIdentifier = "cellIdentifier"
+    let cellName = "PushMessageCell"
+    
+    override func loadView() {
+        let view = UINib(nibName: "PushListController", bundle: nil).instantiate(withOwner: self, options: nil).first
+        if (view != nil) {
+            self.view = view as! UIView!
+        }
+    }
+    
+    override func viewDidLoad() {
+        
+        // レギストセル
+        registCells()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // レギストセル
+    func registCells() {
+        let style1Cell = UINib(nibName: cellName, bundle: nil)
+        self.tableView.register(style1Cell, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ DemoOfSwift3/PushMessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  PushMessageCell.swift
+//  DemoOfSwift3
+//
+//  Created by 许之午 on 2016/12/19.
+//  Copyright © 2016年 许之午. All rights reserved.
+//
+
+import UIKit
+
+class PushMessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
