87ec113624428c7c8681a3b65d0d118067b8c0f6
xuzhiwu <anubis_873375@hotmail.com>
2016-12-19 14:57:06 +0800

    commit changes

--- DemoOfSwift3/LoginViewController.swift
+++ DemoOfSwift3/LoginViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import Foundation
+import NVActivityIndicatorView
 
 class LoginViewController: UIViewController {
     
+    // 二重実行防止
     @IBAction func onClick(_ sender: Any) {
-        print("123")
+        
+        print(NSDate.timeIntervalSinceReferenceDate)
+        
+        // 遅延キャンセル
+        NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(doOnClick), object: nil)
+        // 遅延対応
+        /*
+         1.5秒待ち、doOnClickを呼び出す
+         */
+        self.perform(#selector(doOnClick), with: nil, afterDelay: 1.5)
+    }
+    
+    @objc private func doOnClick() {
+        
+        print(NSDate.timeIntervalSinceReferenceDate)
+        // 詳細表示へ
+        self.performSegue(withIdentifier: "ShowDetail", sender: nil)
     }
     
 }
