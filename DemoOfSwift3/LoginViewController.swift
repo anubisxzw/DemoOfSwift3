5560cd6b22f2d579b5770ac70d1a8c8c5265f91c
xuzhiwu <anubis_873375@hotmail.com>
2016-12-19 14:01:15 +0800

    create

---
+++ DemoOfSwift3/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  DemoOfSwift3
+//
+//  Created by 许之午 on 2016/12/19.
+//  Copyright © 2016年 许之午. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBAction func onClick(_ sender: Any) {
+        print("123")
+    }
+    
+}
